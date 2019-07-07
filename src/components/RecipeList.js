9a4edaf85b2173cf66b3a8d507dc8dfd9c7c98be
EslamElfateh <eslam.elfateh.94@gmail.com>
2019-07-07 21:56:27 +0200

    Dynamic_Stite

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react'
-import Recipe from './Recipe'
+import React, { Component } from "react";
+import Recipe from "./Recipe";
 export default class RecipeList extends Component {
   render() {
+    const { recipes } = this.props;
     return (
-      <div>
-        
-      </div>
-    )
+      <>
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase md-3">
+              <h1 className="text-slanted">recipe list</h1>
+            </div>
+            <div className="row">
+              {recipes.map(recipe => (
+                <Recipe key={recipe.recipe_id} recipe={recipe} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </>
+    );
   }
 }
- 
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
 import RecipeList from "../components/RecipeList";
 import Search from "../components/Search";
+import { recipeData } from "../data/tempList";
+
 export default class Recipes extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    recipes: recipeData,
+    search: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      search: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
   render() {
     return (
       <>
-        <Search />
-        <RecipeList />
+        <Search
+          search={this.state.search}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <RecipeList recipes={this.state.recipes} />
       </>
     );
   }
